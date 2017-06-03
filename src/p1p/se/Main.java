9a2f7894d05dbb9939968c17df8ce25181952455
mattiasbpersson <mattiasbpersson@gmail.com>
2017-06-03 19:47:55 +0200

    Add different executors, run generation in parallell

--- src/p1p/se/Main.java
+++ src/p1p/se/Main.java
@@ -1,29 +1,84 @@
 package p1p.se;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-  public static void main(String... args) throws IOException, URISyntaxException {
-    Path directory = SetupEnvironment.makeItSo(10, 10_000_000);
+  public static void main(String... args)
+      throws IOException, URISyntaxException, InterruptedException {
+    Path directory = SetupEnvironment.makeItSo(10, 100_000_000);
+
+    Main main = new Main();
+
+    main.calculateWithStreamNotParallell(directory);
+
+    ExecutorService service = Executors.newCachedThreadPool();
+    main.calculateWithExecutor(service, directory, "calculateWithCachedThreadPool");
+    service.shutdown();
+
+    service = Executors.newFixedThreadPool(10);
+    main.calculateWithExecutor(service, directory, "calculateWithFixedThreadPool" + 10);
+    service.shutdown();
+
+    service = Executors.newFixedThreadPool(20);
+    main.calculateWithExecutor(service, directory, "calculateWithFixedThreadPool" + 20);
+    service.shutdown();
+
+    service = Executors.newFixedThreadPool(30);
+    main.calculateWithExecutor(service, directory, "calculateWithFixedThreadPool" + 30);
+    service.shutdown();
+
+  }
+
+  private void calculateWithStreamNotParallell(Path directory) throws IOException {
     LocalTime time = LocalTime.now();
 
-    Map<Integer, Long> result = Files.list(directory)
+    Map<Integer, Long> result = flatMapStreamOfStuff(Files.list(directory)
         .filter(p -> p.toString().contains(SetupEnvironment.FILE_NAME))
-        .map(Main::calculate)
-        .map(Map::entrySet)
+        .map(this::calculate));
+    System.out.println("calculateWithStreamNotParallell Time: " + time.until(LocalTime.now(), ChronoUnit.MILLIS));
+    System.out.println("Result: " + result);
+  }
+
+  private void calculateWithExecutor(ExecutorService service, Path directory, String type) throws IOException {
+    LocalTime time = LocalTime.now();
+
+    List<Callable<Map<Integer, Long>>> tasks = Files.list(directory)
+        .filter(p -> p.toString().contains(SetupEnvironment.FILE_NAME))
+        .map(s -> (Callable<Map<Integer, Long>>)() -> calculate(s))
+        .collect(Collectors.toList());
+
+    Map<Integer, Long> result = new HashMap<>();
+    try {
+      result = flatMapStreamOfStuff(service.invokeAll(tasks).stream().map(f -> getResult(f)));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println(type + " Time: " + time.until(LocalTime.now(), ChronoUnit.MILLIS));
+    System.out.println("Result: " + result);
+
+  }
+
+  private Map<Integer, Long> flatMapStreamOfStuff(Stream<Map<Integer, Long>> s){
+    return s.map(Map::entrySet)
         .flatMap(Collection::stream)
         .collect(
             Collectors.toMap(
@@ -31,11 +86,9 @@ public class Main {
                 v -> v.getValue(),
                 (s1, s2) -> s1 + s2
             ));
-    System.out.println("Time: " + time.until(LocalTime.now(), ChronoUnit.MILLIS));
-    System.out.println("Result: " + result);
   }
 
-  private static Map<Integer, Long> calculate(Path path) {
+  private Map<Integer, Long> calculate(Path path) {
     Map<Integer, Long> result = null;
     try {
       result = Files.lines(path).map(s -> Integer.parseInt(s))
@@ -46,4 +99,17 @@ public class Main {
     }
     return result;
   }
+
+  private Map<Integer, Long> getResult(Future<Map<Integer, Long>> future){
+    Map<Integer, Long> r = new HashMap<>();
+    try {
+      r = future.get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+    return r;
+  }
+
 }
--- src/p1p/se/SetupEnvironment.java
+++ src/p1p/se/SetupEnvironment.java
@@ -2,11 +2,13 @@ package p1p.se;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class SetupEnvironment {
@@ -14,10 +16,20 @@ public class SetupEnvironment {
   private static final String DATA_DIR = "data";
   public static final String FILE_NAME = "numbers";
 
-  public static Path makeItSo(int numberOfFiles, int numbersPerFile) throws IOException {
+  public static Path makeItSo(int numberOfFiles, int numbersPerFile)
+      throws IOException, InterruptedException {
     Path directory = createDirectory();
+    ExecutorService service = Executors.newFixedThreadPool(numberOfFiles);
     IntStream.range(1, numberOfFiles)
-        .forEach(n -> generateRandom(Paths.get(DATA_DIR, FILE_NAME + n + ".txt"), numbersPerFile));
+        .forEach(n -> service.submit(() -> generateRandom(Paths.get(DATA_DIR, FILE_NAME + n + ".txt"), numbersPerFile)));
+    service.shutdown();
+    System.out.println("Waiting for files to be generated");
+    while(!service.isTerminated()) {
+      System.out.print(".");
+      service.awaitTermination(1, TimeUnit.SECONDS);
+    }
+    System.out.println("Files created");
+
     return directory;
   }
 
